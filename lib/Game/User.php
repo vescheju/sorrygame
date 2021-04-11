c17c4f703e0362437b2e271e9191333d4e6efb0b
mbhadange <65362340+mbhadange@users.noreply.github.com>
2021-04-11 15:54:55 -0400

    created table class and user class

--- lib/Game/User.php
+++ lib/Game/User.php
@@ -8,6 +8,91 @@ class User
     const ADMIN = "A";
     const STAFF = "S";
     const CLIENT = "C";
+
     const SESSION_NAME = 'user';
 
+    /**
+     * Constructor
+     * @param $row Row from the user table in the database
+     */
+    public function __construct($row) {
+        $this->id = $row['id'];
+        $this->email = $row['email'];
+        $this->name = $row['name'];
+        $this->notes = $row['notes'];
+        $this->joined = strtotime($row['joined']);
+        $this->role = $row['role'];
+    }
+
+    /**
+     * Determine if user is a staff member
+     * @return bool True if user is a staff member
+     */
+    public function isStaff() {
+        return $this->role === self::ADMIN ||
+            $this->role === self::STAFF;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getNotes() {
+        return $this->notes;
+    }
+
+    public function getJoined() {
+        return $this->joined;
+    }
+
+    public function getRole() {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $notes
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    private $id;		// The internal ID for the user
+    private $email;		// Email address
+    private $name; 		// Name as last, first
+    private $notes;		// Notes for this user
+    private $joined;	// When user was added
+    private $role;		// User role
 }
