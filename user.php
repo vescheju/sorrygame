37252366d8a0f27e804fae6c90964beb4724f762
Justin James Vesche <vescheju@msu.edu>
2021-04-12 19:57:13 -0400

    user.php page

---
+++ user.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Sorry Players</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="lib/game.css">
+</head>
+
+<body>
+<div class="user">
+    <div class="nav">
+        <nav>
+            <ul class="left">
+                <li><a href="./">Home</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <header class="main">
+        <h1><img src="images/green.png" alt="Green game piece"> New User Sign Up
+            <img src="images/green.png" alt="Green game piece"></h1>
+    </header>
+
+    <form>
+        <fieldset>
+            <legend>User</legend>
+            <p>Please enter in the following information:</p>
+            <p>
+                <label for="email">Email</label><br>
+                <input type="email" id="email" name="email" placeholder="Email">
+            </p>
+            <p>
+                <label for="name">Name</label><br>
+                <input type="text" id="name" name="name" placeholder="Name">
+            </p>
+            <p>
+                <input type="submit" value="OK"> <input type="submit" value="Cancel">
+            </p>
+
+        </fieldset>
+    </form>
+
+    <footer>
+        <p>Sorry! Team 25</p>
+    </footer>
+
+</div>
+
+</body>
+</html>
