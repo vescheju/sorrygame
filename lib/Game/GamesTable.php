1bbfeae7eea0ef55924eb69be74cc9a59cb5a8ca
Jessica Maye McCoy <mccoyjes@msu.edu>
2021-04-17 20:53:48 -0400

    GameTable and GamesTable classes

---
+++ lib/Game/GamesTable.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace Game;
+
+
+class GamesTable extends Table
+{
+
+    public function __construct(Site $site)
+    {
+        parent::__construct($site, "game");
+    }
+
+    public function get($id)
+    {
+        $sql = <<<SQL
+SELECT * from $this->tableName
+where id=?
+SQL;
+        $pdo = $this->pdo();
+        $statement = $pdo->prepare($sql);
+
+        $statement->execute(array($id));
+        if ($statement->rowCount() === 0) {
+            return null;
+        }
+        return new GameTable($statement->fetch(\PDO::FETCH_ASSOC));
+    }
+
+    public function getState(GameTable $gameTable)
+    {
+        $sql = <<<SQL
+SELECT state from $this->tableName
+where id=?
+SQL;
+        $pdo = $this->pdo();
+        $statement = $pdo->prepare($sql);
+
+        $statement->execute(array($gameTable->getId()));
+        if ($statement->rowCount() === 0) {
+            return null;
+        }
+        $row = $statement->fetch(\PDO::FETCH_ASSOC);
+        $json = $row['state'];
+        return json_decode($json, true);
+    }
+
+    public function setState(GameTable $gameTable)
+    {
+        $json = json_encode($gameTable->getState());
+
+        $sql = <<<SQL
+UPDATE $this->tableName
+SET state=?
+WHERE id=?
+SQL;
+
+        $pdo = $this->pdo();
+        $statement = $pdo->prepare($sql);
+
+        try {
+            $statement->execute(array($json, $gameTable->getId()));
+        }
+        catch(\PDOException $e) {
+            return false;
+        }
+        if($statement->rowCount() === 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public function addPlayer($playerId, $color)
+    {
+        //TO DO
+    }
+}
---
+++ tests/tests/localize.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Function to localize our site
+ * @param $site The Site object
+ */
+return function(Game\Site $site) {
+// Set the time zone
+    date_default_timezone_set('America/Detroit');
+
+    $site->setEmail('bhadang1@msu.edu');
+    $site->setRoot('/~mccoyjes/project2');
+    $site->dbConfigure('mysql:host=mysql-user.cse.msu.edu;dbname=bhadang1',
+        'bhadang1',       // Database user
+        'F00t$ball',     // Database password
+        'test_sorry_');            // Table prefix
+};
