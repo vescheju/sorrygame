4da51b03deae9b01e3fe58a8f7dfb1e70d7dc0bb
pooja <pangurus@msu.edu>
2021-04-19 16:47:23 -0400

    Join game- quick change

--- create-join-game.php
+++ create-join-game.php
@@ -1,13 +1,9 @@
 <?php
-require __DIR__ . '/lib/game.inc.php';
-$view = new Game\StartView($game);
+
 ?>
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Sorry!</title>
-    <link href="lib/game.css" type="text/css" rel="stylesheet" />
 
 </head>
 <body>
